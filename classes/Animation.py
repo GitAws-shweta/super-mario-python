6803a0103bac05530d4bc0b85044467d2a5603e3
GitAws-shweta <vishwakarmashweta344@gmail.com>
2025-09-25 21:14:30 +0530

    Update Animation.py

--- classes/Animation.py
+++ classes/Animation.py
@@ -1037,6 +1037,20 @@ class Spritesheet(object):
             image, (xTileSize * scalingfactor, yTileSize * scalingfactor)
         )
 
+import pygame
+
+
+class Tile:
+    def __init__(self, sprite, rect):
+        self.sprite = sprite
+        self.rect = rect
+
+    def drawRect(self, screen):
+        try:
+            pygame.draw.rect(screen, pygame.Color(255, 0, 0), self.rect, 1)
+        except Exception:
+            pass
+
 
 
 
