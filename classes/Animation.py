6ea88648b993a06e184318bf0d8e4bd8b15a5ab6
GitAws-shweta <vishwakarmashweta344@gmail.com>
2025-09-25 21:12:29 +0530

    Update Animation.py

--- classes/Animation.py
+++ classes/Animation.py
@@ -897,6 +897,23 @@ class Sound:
         self.music_channel.play(music)
 
 
+class Sprite:
+    def __init__(self, image, colliding, animation=None, redrawBackground=False):
+        self.image = image
+        self.colliding = colliding
+        self.animation = animation
+        self.redrawBackground = redrawBackground
+
+    def drawSprite(self, x, y, screen):
+        dimensions = (x * 32, y * 32)
+        if self.animation is None:
+            screen.blit(self.image, dimensions)
+        else:
+            self.animation.update()
+            screen.blit(self.animation.image, dimensions)
+
+
+
 
 
 
